ebbe46ccff25cff829e6b7f76b575dcce2787e11
Gregory Setnik <gsetnik18@gmail.com>
2019-04-18 15:40:01 -0400

    working mostly

---
+++ bamazonCustomer.js
@@ -0,0 +1,89 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var table = require("console.table");
+
+var inventory = [];
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "BuildsADam52!",
+    database: "bamazon"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    readProducts();
+    //createProduct();
+});
+
+var getProducts = function () {
+    inquirer
+        .prompt([
+            /* Pass your questions in here */
+            {
+                type: "input",
+                message: "What would you like to purchase?",
+                name: "id",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        ])
+        .then(answers => {
+            console.log(answers);
+            answers.id;
+            //checks if item id exists
+            for (var i = 0; i < inventory.length; i++) {
+                if (answers.id == inventory[i].item_id) {
+                    quantity(inventory[i]);
+                };
+            }
+        });
+};
+
+var quantity = function (item) {
+    inquirer
+        .prompt([
+            /* Pass your questions in here */
+            {
+                type: "input",
+                message: "How much would you like to purchase?",
+                name: "amount"
+            }
+        ])
+        //responses for quantity
+        .then(answers => {
+            console.log(answers)
+            if (answers.amount <= item.stock_quantity) {
+                console.log("Coming right up!")
+            }
+            else {
+                console.log("We don't have enough in stock.");
+                getProducts();
+            }
+            answers.amount
+        });
+};
+
+function readProducts() {
+    console.log("Selecting all products...\n");
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        console.table(res);
+        inventory = res;
+        getProducts();
+    });
+};
